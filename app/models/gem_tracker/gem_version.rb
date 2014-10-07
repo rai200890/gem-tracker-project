2a49a8ea7c9c2006f5482bc2866091522df3e5f2
Raissa Ferreira <rai200890@gmail.com>
2014-10-07 17:11:05 -0300

    Bug fix in model relationships.

--- app/models/gem_tracker/gem_version.rb
+++ app/models/gem_tracker/gem_version.rb
@@ -1,6 +1,6 @@
 class GemTracker::GemVersion < ActiveRecord::Base
 
-  belongs_to :gem, class_name: "GemTracker::Gem"
-  has_and_belongs_to_many :gemfile_versions, class_name: "GemTracker::GemfileVersions"
+  belongs_to :gem
+  has_and_belongs_to_many :gemfile_versions
 
 end
