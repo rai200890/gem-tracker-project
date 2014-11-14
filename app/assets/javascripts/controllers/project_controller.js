99ad6faa2031f71f57ec8e109747b047c1c1391b
Raissa Ferreira <rai200890@gmail.com>
2014-11-14 17:04:44 -0200

    Menu selection.

--- app/assets/javascripts/controllers/project_controller.js
+++ app/assets/javascripts/controllers/project_controller.js
@@ -1,6 +1,19 @@
-function ProjectController($scope, $state, $stateParams){
-    $scope.states = [{name: ".info", description: "Project Info"},
-        {name: ".branches", description: "Branches"}
-        ,{name: ".diff", description: "Diff"}];
+function ProjectController($rootScope, $scope, $state){
+    $scope.selectedState = null;
+    $scope.states = [{name: "projects_details.info", description: "Project Info"},
+        {name: "projects_details.branches", description: "Branches"}
+        ,{name: "projects_details.diff", description: "Diff"}];
 
+    $scope.states.forEach(function(state, i) {
+        if (state.name == $state.current.name)
+            $scope.selectedState = $scope.states[i];
+    });
+
+    $rootScope.$on('$stateChangeStart',
+        function(event, toState, toParams, fromState, fromParams){
+            $scope.states.forEach(function(state, i) {
+                if (state.name == toState.name)
+                    $scope.selectedState = $scope.states[i];
+            });
+        })
 }
